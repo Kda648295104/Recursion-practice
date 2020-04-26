d0f02b9005108f6247cccf7544b24c0a74df00a8
Kda648295104 <63603361+Kda648295104@users.noreply.github.com>
2020-04-26 19:56:20 +0100

    Update koCh.py

--- koCh.py
+++ koCh.py
@@ -8,6 +8,8 @@ def fill(jug1,jug2,n):
         jug2 = 3 - jug2  #3
         jug1 = jug1 - jug2#1
         if jug1 == n:
+            print("Size of jug1 is 4. Left in jug1 is %d"%jug1)
+            print("Size of jug2 is 3. Left in jug2 is %d before last operation"%(3-jug2))
             return None
         print("Pump out %d gallon from jug2"%jug2)
         print("Pump %d gallon in to jug2 from jug1 again" %jug1)
